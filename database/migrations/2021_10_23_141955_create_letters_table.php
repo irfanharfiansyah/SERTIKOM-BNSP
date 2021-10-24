6840216f84aea62fd1f61e8c633189b659dbdaaa
irfanharfiansyah <irfanharfiansyah37@gmail.com>
2021-10-24 17:04:36 +0700

    Add Migration

---
+++ database/migrations/2021_10_23_141955_create_letters_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLettersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('letters', function (Blueprint $table) {
+            $table->id();
+            $table->string('number');
+            $table->foreignId('category_id');
+            $table->string('title');
+            $table->string('filename');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('letters');
+    }
+}
